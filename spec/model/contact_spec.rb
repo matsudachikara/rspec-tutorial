3d3608d5b9a6c9f85ddd6956a221bd1fce0c2fb6
Chikara Matsuda <matsudachikara@yahoo.co.jp>
2016-11-27 23:42:48 +0900

    メールアドレスが重複する場合のテスト作成完了

--- spec/model/contact_spec.rb
+++ spec/model/contact_spec.rb
@@ -2,15 +2,42 @@ require 'rails_helper'
 
 describe Contact do
   # 姓と名とメールがあれば有効な状態であること
-  it "is valid with a firstname, lastname and email"
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com'
+    )
+    expect(contact).to be_valid
+  end
   # 名がなければ無効な状態であること
-  it "is invalid without a firstname"
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
   # 姓がなければ無効な状態であること
-  it "is invalid without a lastname"
+  it "is invalid without a lastname" do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
   # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address"
+  it "is invalid without an email address" do
+  end
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+        firstname: 'Joe', lastname: 'Tester',
+        email: 'tester@example.com'
+    )
+    contact = Contact.create(
+        firstname: 'Jane', lastname: 'Tester',
+        email: 'tester@example.com'
+    )
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
+  end
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string"
 end
