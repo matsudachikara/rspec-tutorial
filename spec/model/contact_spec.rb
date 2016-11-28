86df44bc4eca618c78c3b6305e560849979b1473
Chikara Matsuda <matsudachikara@yahoo.co.jp>
2016-11-29 00:59:51 +0900

    rspec testing all done!

--- spec/model/contact_spec.rb
+++ spec/model/contact_spec.rb
@@ -43,4 +43,37 @@ describe Contact do
     contact = Contact.new(firstname: 'John', lastname: 'Doe', email: 'johndoe@example.com')
     expect(contact.name).to eq 'John Doe'
   end
+
+  describe "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(
+          firstname: 'John',
+          lastname: 'Smith',
+          email: 'jsmith@example.com'
+      )
+      @jones = Contact.create(
+          firstname: 'Tim',
+          lastname: 'Jones',
+          email: 'tjones@example.com'
+      )
+      @johnson = Contact.create(
+          firstname: 'John',
+          lastname: 'Johnson',
+          email: 'jjohnson@example.com'
+      )
+    end
+
+    context "matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("j")).to eq [@johnson, @jones]
+      end
+    end
+
+    context "non-matching leters" do
+      it "omits results that do not match" do
+        expect(Contact.by_letter("j")).not_to include @smith
+      end
+    end
+
+  end
 end
